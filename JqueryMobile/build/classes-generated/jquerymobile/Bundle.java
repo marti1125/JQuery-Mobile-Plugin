af8bb0d01c702c57d52a64c84d760fdc25b27ce1
Willy <marti1003@hotmail.com>
2012-07-12 00:57:38 -0500

    first commit

---
+++ JqueryMobile/build/classes-generated/jquerymobile/Bundle.java
@@ -0,0 +1,13 @@
+package jquerymobile;
+/** Localizable strings for {@link jquerymobile}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>JqueryMobile</i>
+     * @see JqueryMobileWizardIterator
+     */
+    static String JqueryMobile_displayName() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "JqueryMobile_displayName");
+    }
+    private void Bundle() {}
+}
