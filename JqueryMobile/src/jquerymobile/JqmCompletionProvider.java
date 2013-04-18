bd2d18d4b99faa156111a94e7bd158643abe36a1
Willy Aguirre <marti1003@hotmail.com>
2013-04-18 15:56:31 -0500

    Implementing the createTask Method

--- JqueryMobile/src/jquerymobile/JqmCompletionProvider.java
+++ JqueryMobile/src/jquerymobile/JqmCompletionProvider.java
@@ -5,10 +5,14 @@ package jquerymobile;
  * @author willy
  */
 
+import javax.swing.text.Document;
 import javax.swing.text.JTextComponent;
 import org.netbeans.api.editor.mimelookup.MimeRegistration;
 import org.netbeans.spi.editor.completion.CompletionProvider;
+import org.netbeans.spi.editor.completion.CompletionResultSet;
 import org.netbeans.spi.editor.completion.CompletionTask;
+import org.netbeans.spi.editor.completion.support.AsyncCompletionQuery;
+import org.netbeans.spi.editor.completion.support.AsyncCompletionTask;
 
 @MimeRegistration(mimeType = "text/html", service = CompletionProvider.class)
 public class JqmCompletionProvider implements CompletionProvider {
@@ -18,13 +22,24 @@ public class JqmCompletionProvider implements CompletionProvider {
     }
 
     @Override
-    public CompletionTask createTask(int i, JTextComponent jtc) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public CompletionTask createTask(int queryType, JTextComponent jtc) {
+        
+        if(queryType != CompletionProvider.COMPLETION_QUERY_TYPE){
+            return null;
+        }
+        
+        return new AsyncCompletionTask(new AsyncCompletionQuery() {
+
+            @Override
+            protected void query(CompletionResultSet crs, Document dcmnt, int i) {
+                
+            }
+        });
     }
 
     @Override
     public int getAutoQueryTypes(JTextComponent jtc, String string) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
 }
